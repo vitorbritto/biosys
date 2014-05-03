93200128ccc29fecbae910629af2bbc580515406
Vitor Britto <code@vitorbritto.com.br>
2014-05-03 00:35:37 -0300

    Release version 0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-biosys
-======
+# Biosys
 
 Returns information about your system.
+
+## Install
+
+    npm install biosys --save-dev
+
+## Usage
+
+```javascript
+var biosys = require('biosys');
+biosys();
+```
---
+++ index.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright 2014, All Rights Reserved.
+ *
+ * Code licensed under the MIT License:
+ * https://github.com/vitorbritto/biosys/blob/master/LICENSE
+ *
+ * Author: Vitor Britto <code@vitorbritto.com.br>
+ */
+
+require('./lib/biosys');
